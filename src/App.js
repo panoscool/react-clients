6398fa431b94b22e1db1f23ded4226a2101c7f66
Vladi K <vladi_lk@hotmail.com>
2018-04-13 16:19:42 +0400

    Adding the Google button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GoogleLogin from 'react-google-login';
 import './App.css';
 
 class App extends Component {
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <GoogleLogin
+          clientId="411211794868-4mbdl766r7eqa1trb3g27hnme7rsia0d.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
       </div>
     );
   }
