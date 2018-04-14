6d323f99e7045b117606570d45126d0443e953ed
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-04-14 21:56:40 +0300

    Basic authentication structure

--- src/App.js
+++ src/App.js
@@ -2,20 +2,45 @@ import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
 import './App.css';
 
+import Authentication from './services/authentication';
+
 class App extends Component {
 
-  render() {
-    const responseGoogle = (response) => {
-      console.log(response);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authenticated: Authentication.exists(), // boolean true or false
+      user: Authentication.getUserData(), // either an object or null
     }
+  }
+
+  render() {
+    const { authenticated, user } = this.state;
+
     return (
       <div className="App">
-        <GoogleLogin
-          clientId="411211794868-2kj7ooa8fm0gu41eqn6596m62tfiklro.apps.googleusercontent.com"
-          buttonText="Login"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-        />
+
+        {authenticated ?
+          <div>
+            <div>
+              <div><img src={user.imageUrl} alt="Profile Image" /></div>
+              <div>{user.name}</div>
+            </div>
+
+            <div>
+              <button onClick={() => Authentication.destroy()}>Log out</button>
+            </div>
+          </div>
+          :
+          <GoogleLogin
+            clientId="411211794868-2kj7ooa8fm0gu41eqn6596m62tfiklro.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={Authentication.success}
+            onFailure={Authentication.failure}
+          />
+        }
+
       </div>
     );
   }
---
+++ src/services/authentication.js
@@ -0,0 +1,54 @@
+const storage = localStorage;
+const authenticationKey = 'auth';
+
+export default class Authentication {
+
+  /**
+   * Retrieves logged in user data or null, if he's not logged in
+   */
+  static getUserData() {
+    const profileData = storage.getItem(authenticationKey);
+
+    return profileData ? JSON.parse(profileData) : null;
+  }
+
+  /**
+   * Checks whether the user has logged in or not
+   */
+  static exists() {
+    return storage.getItem(authenticationKey) ? true : false;
+  }
+
+  /**
+   * Receives a profile object and properly persists it on defined storage
+   */
+  static authenticate(data) {
+    const json = JSON.stringify(data);
+    storage.setItem(authenticationKey, json);
+  }
+
+  /**
+   * Destroys current user session by simply deleting storage entry
+   */
+  static destroy() {
+    storage.removeItem(authenticationKey);
+  }
+
+  /**
+   * Action called after proper authentication
+   */
+  static success(response) {
+    console.log('Authentication', 'Success', response);
+
+    // I only use that part, but you can use anything you'd like
+    Authentication.authenticate(response.profileObj);
+  }
+
+  /**
+   * Action called after failed authentication attempt
+   */
+  static failure(response) {
+    console.error('Authentication', 'failure', response);
+  }
+
+}
