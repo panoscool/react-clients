a22ee9c3651a28be06d9bbc0ae77c1d25c098e8d
Vladi K <vladi_lk@hotmail.com>
2018-04-14 15:02:31 +0400

    getting user profile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GoogleLogin from 'react-google-login';
 import './App.css';
 
 class App extends Component {
+
   render() {
     const responseGoogle = (response) => {
       console.log(response);
@@ -10,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <GoogleLogin
-          clientId="411211794868-4mbdl766r7eqa1trb3g27hnme7rsia0d.apps.googleusercontent.com"
+          clientId="411211794868-2kj7ooa8fm0gu41eqn6596m62tfiklro.apps.googleusercontent.com"
           buttonText="Login"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
